7ff1f9febda7cd7a9f3428db72632cf5876ef1ec
Wesh-Mill <weshmill223@gmail.com>
2025-07-30 05:44:07 +0000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,34 @@
+const header = document.Selector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+})
+
+function login() {
+                const username = document.getElementById('username').value;
+                const password = document.getElementById('password').value;
+                const ownerUsername = 'its_weshmill';
+                const ownerPassword = 'Wesh0404Mill';
+                if (username === ownerUsername && password === ownerPassword) {
+                    alert('Login successful! Welcome, ' + username + '!');
+                    localStorage.setItem('isOwner', "true");
+                    window.location.href = 'add-product.html';
+                } else {
+                    alert("Access denied!");
+                }
+
+                return false;
+}
+function logout() {
+    if(localStorage.getItem("isOwner") !== "true") {
+        alert("Access denied. Only the owner can access this page.");
+    window.location.href = 'index.html';
+    }
+}
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onlick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
